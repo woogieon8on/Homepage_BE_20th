847e70d97efe2c119753f4806bf9dc7588aa2bcf
kjiyun <112507402+kjiyun@users.noreply.github.com>
2023-08-05 21:09:56 +0900

    #1 Fix : remove ordercompleteserializer in ticket.views (#10)
    * #2 feat: add portone api
    
    * #12 feat: add swagger
    
    * #2 feat: modify ticket model and add general ticket view
    
    * #1 feat: add ticket checkout and validation function
    
    * #1 feat: add requirements.txt 8/1
    
    * #4 feat: add applyform create view
    
    * #1 Fix: remove ordercompleteserializer in ticket.views

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import GeneralTicketOrderView, OrderCheckoutView, OrderValidationView
+from .views import FreshmanTicketOrderView, GeneralTicketOrderView, OrderCheckoutView, OrderValidationView
 
 app_name = 'tickets'
 
 urlpatterns = [
     path('general_ticket/', GeneralTicketOrderView.as_view({'post':'create'}), name='general_ticket'),
+    path('freshman_ticket/', FreshmanTicketOrderView.as_view({'post':'create'}), name='freshman_ticket'),
     path('checkout/', OrderCheckoutView.as_view({'post':'post'}), name='order_checkout'),
     path('validation/', OrderValidationView.as_view({'post':'post'}), name='order_validation'),
     path('complete/', GeneralTicketOrderView.as_view({'get':'get'}), name='order_complete'),
