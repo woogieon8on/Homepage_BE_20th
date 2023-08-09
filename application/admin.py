74870f5d9f3e7014ddf198bdfb0db6ecd620f988
SeoyeonPark1223 <137621961+SeoyeonPark1223@users.noreply.github.com>
2023-08-09 12:46:04 +0900

    #17 Feat: connect excel (#18)

--- application/admin.py
+++ application/admin.py
@@ -8,16 +8,4 @@ from import_export.admin import ExportActionMixin
 class ApplyFormAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = ('name','phone_num','birthdate','gender','address','first_preference',
                     'second_preference','play_instrument','motive')
-    #pass
-    
-"""
-    name = models.CharField(max_length=20)   # 지원자 이름
-    phone_num = models.CharField(max_length=13, null=True)   # 전화번호('-'없는 상태로 입력받기)
-    birthdate = models.DateField(blank=True)   # 생년월일
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=20, blank=True)   # 성별
-    address = models.CharField(max_length=200)   # 거주지
-    first_preference = models.CharField(choices=PREFERENCE_CHOICES, max_length=20)   # 1지망
-    second_preference = models.CharField(choices=PREFERENCE_CHOICES, max_length=20)   # 2지망
-    play_instrument = models.CharField(max_length=200, blank=True)   # 다룰 줄 아는 악기
-    motive = models.TextField()   # 지원 동기
-"""
+    pass
--- tickets/admin.py
+++ tickets/admin.py
@@ -1,18 +1,50 @@
 from django.contrib import admin
 from . import models
+from import_export.admin import ExportActionMixin
+from import_export import fields, resources
+from import_export.widgets import ForeignKeyWidget
+from .models import Participant, FreshmanTicket, GeneralTicket
 
 # Register your models here.
 
-@admin.register(models.GeneralTicket)
-class GeneralTicketAdmin(admin.ModelAdmin):
+class ParticipantResource(resources.ModelResource):
+    general_ticket_buyer = fields.Field(
+        column_name='general_ticket_buyer',
+        attribute='general_ticket__buyer',  # ForeignKey의 buyer 필드에 접근
+    )
+
+    class Meta:
+        model = Participant
+        exclude = ('id',)  # id 필드 제외
+        fields = ('name', 'phone_num', 'general_ticket_buyer')
+
+class FreshmanTicketResource(resources.ModelResource):
+    class Meta:
+        model = FreshmanTicket
+        exclude = ('updated',)  # updated 필드 제외
+
+class GeneralTicketResource(resources.ModelResource):
+    class Meta:
+        model = GeneralTicket
+        exclude = ('updated',)  # updated 필드 제외
+        
+
+@admin.register(models.Participant)
+class ParticipantAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ('name','phone_num')
+    resource_class = ParticipantResource  # ParticipantResource를 사용하도록 설정
     pass
 
 @admin.register(models.FreshmanTicket)
-class FreshmanTicketAdmin(admin.ModelAdmin):
+class FreshmanTicketAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ('buyer','phone_num','major','student_id','meeting')
+    resource_class = FreshmanTicketResource  # FreshmanTicketResource 설정
     pass
 
-@admin.register(models.Participant)
-class ParticipantAdmin(admin.ModelAdmin):
+@admin.register(models.GeneralTicket)
+class GeneralTicketAdmin(ExportActionMixin, admin.ModelAdmin):
+    list_display = ('buyer','phone_num','member','price')
+    resource_class = GeneralTicketResource  # GeneralmanTicketResource 설정
     pass
 
 @admin.register(models.OrderTransaction)
