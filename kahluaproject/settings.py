761a3e1d3bb71d06e290a05c2413556adab5886d
woogieon8on <140885810+woogieon8on@users.noreply.github.com>
2023-11-16 20:07:41 +0900

    #41 Feat: key error & 502 error correction (#42)
    * kahluaband#41 Fix: key error correction
    
    * kahluaband#41 Fix: key error correction
    
    * kahluaband#41 Fix: key error correction
    
    * kahluaband#41 Fix: key error correction
    
    * kahluaband#41 Feat: add kahluaproject.ini in .config/uwsgi
    
    * kahluaband#41 Feat: nginx configuration update
    
    * kahluaband#41 Fix: 502 error correction
    
    * kahluaband#41 Fix: key error correction

--- kahluaproject/settings.py
+++ kahluaproject/settings.py
@@ -13,6 +13,9 @@ from datetime import timedelta
 from pathlib import Path
 import os
 import environ
+import django
+from django.utils.translation import gettext
+django.utils.translation.ugettext = gettext
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,7 +95,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+    # 'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'kahluaproject.urls'
