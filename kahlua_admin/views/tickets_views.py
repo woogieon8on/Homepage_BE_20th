b9a65a852e7d0417c57441c3fa67f493798c877e
kjiyun <112507402+kjiyun@users.noreply.github.com>
2024-01-06 01:20:01 +0300

    #34 Feat: add swagger and fix order name (#54)
    * #1 Fix: fix swagger2
    
    * #1 Feat: check phone number when checking order
    
    * #34 Feat: Add general ticket admin and modify swagger
    
    * #1 Feat: modify freshman ticket checking method
    
    * #34 Feat: add function sorting list by name
    
    * #50 Feat: add status field in general ticket
    
    * #50 Feat: add payment field
    
    * #52 Feat: add kahlua superuser
    
    * #34 Feat: add swagger and fix latest to name
    
    ---------
    
    Co-authored-by: 강지윤 <kjiyun@gangjiyun-ui-MacBookAir.local>

--- kahlua_admin/views/tickets_views.py
+++ kahlua_admin/views/tickets_views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets, status, serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from tickets.models import FreshmanTicket, OrderTransaction, Participant
 from ..serializers.tickets_serializers import FreshmanAdminSerializer, GeneralTicketAdminListSerializer
@@ -15,19 +16,43 @@ class TicketsPagination(PageNumberPagination):
 
 
 class FreshmanViewSet(viewsets.ModelViewSet):
-    """
-        새내기 티켓 예약 리스트 가져오기
-    """
     queryset = FreshmanTicket.objects.all().order_by('-id')  #예약 순서대로 정렬
     serializer_class = FreshmanAdminSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = TicketsPagination
 
-    @swagger_auto_schema(operation_id='신입생 예매 티켓 리스트 가져오기')
+    @swagger_auto_schema(
+        operation_id='신입생 티켓 예약 리스트 가져오기',
+        operation_description='''
+            모든 신입생 티켓을 리스트로 불러옵니다.<br/>
+            기본 정렬은 최신이 가장 위로 올라오도록 정렬됩니다. query params에서 name을 True로 설정하면 이름 순으로 정렬됩니다.<br/>
+        ''',
+        responses={
+            "200": openapi.Response(
+                description="OK",
+                examples={
+                    "application/json": {
+                        "status": "success",
+                        "data": {
+                            "id": 1,
+                            "buyer": "kahlua",
+                            "phone_num": "01012345678",
+                            "major": "컴퓨터공학과",
+                            "student_id": "C41111",
+                            "meeting": True
+                        },
+                    }
+                }
+            ),
+            "400": openapi.Response(
+                description="Bad Request",
+            ),
+        }
+    )
     def list(self, request, *args, **kwargs):
         order = self.get_queryset()
-        latest = request.GET.get('latest', False)
-        if latest:
+        order_name = request.GET.get('name', False)
+        if order_name:
             order = FreshmanTicket.objects.all().order_by('buyer')
         serializer = self.get_serializer(order, many=True)
 
@@ -38,19 +63,43 @@ class FreshmanViewSet(viewsets.ModelViewSet):
     
 
 class GeneralTicketListViewSet(viewsets.ModelViewSet):
-    """
-        일반 티켓 예매 정보 가져오기
-    """
     queryset = OrderTransaction.objects.all().order_by('-id')
     serializer_class = GeneralTicketAdminListSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = TicketsPagination
 
-    @swagger_auto_schema(operation_id='일반 예매 티켓 리스트 가져오기')
+    @swagger_auto_schema(
+        operation_id='일반 티켓 예매 정보 가져오기',
+        operation_description='''
+            모든 티켓을 리스트로 불러옵니다.<br/>
+            기본 정렬은 최신이 가장 위로 올라오도록 정렬됩니다. query params에서 name을 True로 설정하면 이름 순으로 정렬됩니다.<br/>
+        ''',
+        responses={
+            "200": openapi.Response(
+                description="OK",
+                examples={
+                    "application/json": {
+                        "status": "success",
+                        "data": {
+                            "id": 1,
+                            "buyer": "kahlua",
+                            "phone_num": "01012345678",
+                            "member": 2,
+                            "merchant_order_id": "734ea4eadf",
+                            "transaction_status": "paid"
+                        },
+                    }
+                }
+            ),
+            "400": openapi.Response(
+                description="Bad Request",
+            ),
+        }
+    )
     def list(self, request, *args, **kwargs):
         order = self.get_queryset()
-        latest = request.GET.get('latest', False)
-        if latest:
+        order_name = request.GET.get('name', False)
+        if order_name:
             order = OrderTransaction.objects.all().order_by('order__buyer')
         serializer = self.get_serializer(order, many=True)
 
